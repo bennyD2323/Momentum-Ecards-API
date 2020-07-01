bb1623a6a93f16d03b0b1788eeb8b3397ff5b690
Benjamin D Adams <badams888@hotmail.com>
2020-07-01 13:49:23 -0400

    setup

--- ecardsapi/asgi.py
+++ ecardsapi/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ecardsAPI project.
+ASGI config for ecardsapi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecardsAPI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecardsapi.settings')
 
 application = get_asgi_application()
--- ecardsapi/wsgi.py
+++ ecardsapi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ecardsAPI project.
+WSGI config for ecardsapi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecardsAPI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecardsapi.settings')
 
 application = get_wsgi_application()
