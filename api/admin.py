15074fd105cfe78578eb03411d9f3c52a3ebd7cb
Benjamin D Adams <badams888@hotmail.com>
2020-06-30 22:18:32 -0400

    admin and settings added stuff'

---
+++ api/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from rest_framework.authtoken.models import Token
+from .models import User, Card
+admin.site.register(Card)
+admin.site.register(User)
--- ecardsAPI/settings.py
+++ ecardsAPI/settings.py
@@ -142,10 +142,11 @@ INTERNAL_IPS = [
     '127.0.0.1',
     # ...
 ]
-
+# why didn't rest_framework.permissions.IsAuthenticatedOrReadOnly work?
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES':
-    ['rest_framework.permissions.IsAuthenicatedOrReadyOnly'],
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.IsAuthenicated'
+    ],
 
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.TokenAuthentication',
